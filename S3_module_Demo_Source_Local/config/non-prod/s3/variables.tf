5ed491e937c14d1d77234e2c493178ea27f35a9a
kumravib <49126207+kumravib@users.noreply.github.com>
2024-10-03 13:17:26 +0530

    Update variables.tf

--- S3_module_Demo_Source_Local/config/non-prod/s3/variables.tf
+++ S3_module_Demo_Source_Local/config/non-prod/s3/variables.tf
@@ -1,11 +1,11 @@
 variable "bucket" {
-    default = "rkb-bucket"
+    default = "rkb-bucket1"
 }
 variable "env" {
     default = "non-prod"
 }
 
 variable "region" {
-    default = "us-east-1"
+    default = "us-east-2"
   
 }
